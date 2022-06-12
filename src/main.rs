ee15468f2adfd3faf04ea041ddbb131432c053c4
Oscar Sandford <oscarsandford@gmail.com>
2022-06-11 20:02:27 -0700

    Storing some changes.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use printpdf::{
 	PdfDocument, Image, 
 	Mm, ImageTransform, ImageXObject, ColorBits, ColorSpace, Px,
 };
+// Use the version of the "image" crate that comes 
+// with printpdf to avoid mismatching identifiers.
 use printpdf::image_crate::*;
 use std::fs::File;
 use std::io::Write;
@@ -17,6 +19,7 @@ const OUT_DIR: &'static str = "./out";
 const API_ENDPT: &'static str = "https://api.mangadex.org/at-home/server";
 const EXAMPLE_CHAPTER_ID: &'static str = "a54c491c-8e4c-4e97-8873-5b79e59da210";
 
+const PX_TO_MM_FACTOR: f64 = 0.2645833;
 
 
 /// A CLI tool for binding manga from MangaDex.
@@ -61,7 +64,7 @@ struct Chapter {
 fn get_pages(client: Client, pages: Vec<String>, label: &str) -> Result<(), Box<dyn std::error::Error>> {
 	println!("Saving {} pages for {}..", pages.len(), label);
 
-	let (doc, mut curr_page, mut curr_layer) = PdfDocument::new(label, Mm(582.), Mm(582.), "Layer 1");
+	let (doc, mut curr_page, mut curr_layer) = PdfDocument::new(label, Mm(10.), Mm(10.), "Layer 0");
 
 	for (i, page) in pages.iter().enumerate() {
 		println!("Working on page {}:", i+1);
@@ -70,7 +73,6 @@ fn get_pages(client: Client, pages: Vec<String>, label: &str) -> Result<(), Box<
 			.send()?
 			.bytes()?;
 		
-
 		// let len = page.len();
 		// let filename = format!("{}/{}_{}.{}", OUT_DIR, label, i, &page[len-3..]);
 		// img.save(&filename)?;
@@ -78,14 +80,30 @@ fn get_pages(client: Client, pages: Vec<String>, label: &str) -> Result<(), Box<
 
 		println!("\t bytes to dynimg");
 
+		println!("\t Creating new page and layer.");
+		
+		println!("------");
+
 		let dyn_img: DynamicImage = load_from_memory(&img_bytes)?;
 		
 		let ctype = dyn_img.color();
 		let data = dyn_img.as_bytes().to_vec();
 
+
+		let width_px: u32 = dyn_img.width();
+		let height_px: u32 = dyn_img.height();
+
+		let width_mm: f64 = dyn_img.width() as f64 * PX_TO_MM_FACTOR;
+		let height_mm: f64 = dyn_img.height() as f64 * PX_TO_MM_FACTOR;
+
+		(curr_page, curr_layer) = doc.add_page(Mm(width_mm), Mm(height_mm), format!("Layer {}", i+1));
+
+		// https://github.com/fschutt/printpdf/blob/v0.5.0/src/xobject.rs#L223
 		let imgx = ImageXObject {
-			width: Px(dyn_img.width() as usize),
-			height: Px(dyn_img.height() as usize),
+			// TODO: might need to insert a factor here because 
+			// the library changes Px weird...  
+			width: Px(width_px as usize),
+			height: Px(height_px as usize),
 			color_space: ColorSpace::from(ctype),
 			bits_per_component: ColorBits::from(ctype),
 			image_data: data,
@@ -94,15 +112,17 @@ fn get_pages(client: Client, pages: Vec<String>, label: &str) -> Result<(), Box<
 			clipping_bbox: None,
 		};
 
+		println!("w:{} h:{}, ", width_px, height_px);
+		dbg!(Px(width_px as usize));
+		dbg!(Px(height_px as usize));
+
 		let imgp = Image::from(imgx);
 
 		println!("\t Adding image to PDF.");
 		let layer = doc.get_page(curr_page).get_layer(curr_layer);
 		imgp.add_to_layer(layer.clone(), ImageTransform::default());
 
-		println!("\t Creating new page and layer.");
-		(curr_page, curr_layer) = doc.add_page(Mm(582.), Mm(582.), format!("Layer {}", i+1));
-		println!("------");
+		
 	}
 
 	
