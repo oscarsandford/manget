8c926f566528ec90ed8eac956b20ca7cd4e1f008
Oscar Sandford <oscarsandford@gmail.com>
2022-06-14 18:52:58 -0700

    Moved parse functions to MDClient methods.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod bind;
 mod parse;
 
 use bind::bind_pages;
-use parse::{Args, MDClient, get_chapter_pages, get_manga_chapters, get_chapter_id, get_label};
+use parse::{Args, MDClient, get_label};
 
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -10,19 +10,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let client = MDClient::new();
 
 	// Get manga chapters from the swagger API.
-	let chapters = get_manga_chapters(&client, "21f54bc1-aefd-4be1-8284-5858b1df0e55")?;
+	let chapters = client.get_manga_chapters("21f54bc1-aefd-4be1-8284-5858b1df0e55")?;
 
 	dbg!(&chapters.len());
 
 	// Grab a specific chapter we want to bind.
 	// Chapter is specified in args!
-	let id = get_chapter_id(&client, &args, chapters)?;
+	let id = client.get_chapter_id(&args, chapters)?;
 	
 	dbg!(&id);
 
 	if id != "err" {
 		// Retrieve this chapter's pages from the at-home API.
-		let pages = get_chapter_pages(&client, &args, id)?;
+		let pages = client.get_chapter_pages(&args, id)?;
 		// Bind them to a PDF.
 		bind_pages(&client, pages, get_label(&args))?;
 	}
--- src/parse.rs
+++ src/parse.rs
@@ -72,98 +72,97 @@ impl MDClient {
 		self.client.get(query).send()
 	}
 	// TODO: maybe make specific methods for fetching chapter or manga itself? To make interface cleaner.
-}
 
+	/*
+	Use the reqwest client to retrieve the pages of a manga chapter given its ID.
+	We can tell it which server to pull from based on the Args.
+	*/
+	pub fn get_chapter_pages(&self, args: &Args, chapter_id: String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+		// This request is blocking.
+		let response = self
+			.fetch(&format!("{}/at-home/server/{}", API_URL, chapter_id))?
+			.json::<ChapterPagesData>()?;
+		
+		let (imgs, quality) = if args.fast { 
+			(response.chapter.dataSaver, "data-saver")
+		} else { 
+			(response.chapter.data, "data")
+		};
+		
+		Ok(imgs.iter().map(|img| format!(
+			"{}/{}/{}/{}",
+			response.baseUrl, quality, response.chapter.hash, img
+		)).collect())
+	}
 
-/*
-Use the reqwest client to retrieve the pages of a manga chapter given its ID.
-We can tell it which server to pull from based on the Args.
-*/
-pub fn get_chapter_pages(client: &MDClient, args: &Args, chapter_id: String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
-	// This request is blocking.
-	let response = client
-		.fetch(&format!("{}/at-home/server/{}", API_URL, chapter_id))?
-		.json::<ChapterPagesData>()?;
-	
-	let (imgs, quality) = if args.fast { 
-		(response.chapter.dataSaver, "data-saver")
-	} else { 
-		(response.chapter.data, "data")
-	};
-	
-	Ok(imgs.iter().map(|img| format!(
-		"{}/{}/{}/{}",
-		response.baseUrl, quality, response.chapter.hash, img
-	)).collect())
-}
+	/*
+	Given a vector of Chapters, retrieve the chapter ID with the given number and target language.
+	*/
+	pub fn get_chapter_id(&self, args: &Args, chapters: Vec<Chapter>) -> Result<String, Box<dyn std::error::Error>> {
+		for chp in chapters.into_iter() {
+			if chp.name == args.chapter {
+				dbg!(&chp);
+				for id in chp.ids {
+					let response = self
+						.fetch(&format!("{}/chapter/{}", API_URL, id))?
+						.json::<serde_json::Value>()?;
+		
+					let lang = response["data"]["attributes"]["translatedLanguage"].as_str().unwrap().to_string();
 
-/*
-Given a vector of Chapters, retrieve the chapter ID with the given number and target language.
-*/
-pub fn get_chapter_id(client: &MDClient, args: &Args, chapters: Vec<Chapter>) -> Result<String, Box<dyn std::error::Error>> {
-	for chp in chapters.into_iter() {
-		if chp.name == args.chapter {
-			dbg!(&chp);
-			for id in chp.ids {
-				let response = client
-					.fetch(&format!("{}/chapter/{}", API_URL, id))?
-					.json::<serde_json::Value>()?;
-	
-				let lang = response["data"]["attributes"]["translatedLanguage"].as_str().unwrap().to_string();
+					dbg!(&lang);
 
-				dbg!(&lang);
-
-				if lang == args.language {
-					return Ok(id);
+					if lang == args.language {
+						return Ok(id);
+					}
 				}
 			}
 		}
+		// Replace this ASAP.
+		Ok("err".to_string())
 	}
-	// Replace this ASAP.
-	Ok("err".to_string())
-}
 
-/*
-Retrieve the manga chapters for a given manga ID from the swagger API.
-*/
-pub fn get_manga_chapters(client: &MDClient, manga_id: &str) -> Result<Vec<Chapter>, Box<dyn std::error::Error>> {
-	let response = client.
-		fetch(&format!("{}/manga/{}/aggregate", API_URL, manga_id))?
-		.json::<serde_json::Value>()?;
+	/*
+	Retrieve the manga chapters for a given manga ID from the swagger API.
+	*/
+	pub fn get_manga_chapters(&self, manga_id: &str) -> Result<Vec<Chapter>, Box<dyn std::error::Error>> {
+		let response = self
+			.fetch(&format!("{}/manga/{}/aggregate", API_URL, manga_id))?
+			.json::<serde_json::Value>()?;
 
-	let mut chapters = Vec::<Chapter>::new();
-	for (vol, chp_data) in response["volumes"].as_object().unwrap() {
-		for (chp, data) in chp_data["chapters"].as_object().unwrap() {
-			// println!("\t{}: {}", chp, data);
+		let mut chapters = Vec::<Chapter>::new();
+		for (vol, chp_data) in response["volumes"].as_object().unwrap() {
+			for (chp, data) in chp_data["chapters"].as_object().unwrap() {
+				// println!("\t{}: {}", chp, data);
 
-			// TODO: see if we can do this better without mutability.
-			let mut ids = vec![data["id"].as_str().unwrap().to_string()];
-			for id in data["others"].as_array().unwrap() {
-				ids.push(id.as_str().unwrap().to_string());
+				// TODO: see if we can do this better without mutability.
+				let mut ids = vec![data["id"].as_str().unwrap().to_string()];
+				for id in data["others"].as_array().unwrap() {
+					ids.push(id.as_str().unwrap().to_string());
+				}
+
+				chapters.push(Chapter{
+					volume: vol.to_string(),
+					name: chp.to_string(),
+					ids: ids,
+				});	
 			}
-
-			chapters.push(Chapter{
-				volume: vol.to_string(),
-				name: chp.to_string(),
-				ids: ids,
-			});	
 		}
+
+		// Note that the chapters will not be in the correct order in the vector, so we 
+		// will need to either sort them beforehand if we want to bind a whole volume, or 
+		// do that when merging the PDF docs (if that's how we decide to do that).
+
+		// Further, while the use of a vector is a good generic start, it means that we have 
+		// to run the length of the vector in order to find a chapter in the worst case.
+		// A HashMap would help solve this problem, but then we need to decide whether the 
+		// key will be a volume or a chapter.
+		// A decision like this is better made down the line, so for now, we will just 
+		// query the vector for a Chapter with a given ID, or the Chapters of a given volume.
+
+		// dbg!(&chapters);
+
+		Ok(chapters)
 	}
-
-	// Note that the chapters will not be in the correct order in the vector, so we 
-	// will need to either sort them beforehand if we want to bind a whole volume, or 
-	// do that when merging the PDF docs (if that's how we decide to do that).
-
-	// Further, while the use of a vector is a good generic start, it means that we have 
-	// to run the length of the vector in order to find a chapter in the worst case.
-	// A HashMap would help solve this problem, but then we need to decide whether the 
-	// key will be a volume or a chapter.
-	// A decision like this is better made down the line, so for now, we will just 
-	// query the vector for a Chapter with a given ID, or the Chapters of a given volume.
-
-	// dbg!(&chapters);
-
-	Ok(chapters)
 }
 
 /*
